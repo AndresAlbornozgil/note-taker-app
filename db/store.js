167f3a61379eb991b4f830c830199aaa1befbb47
AA <aalborgil002@gmail.com>
2024-07-01 18:36:56 -0400

    Added functions to store.js file

--- db/store.js
+++ db/store.js
@@ -6,9 +6,41 @@ const writeFile = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return 
+        return readFile('db/db.json', 'utf8'); //not sure
     }
     write(note) {
         return writeFile('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        this.read()
+            .then((notes) => {
+                let getNotes;
+                if (notes == null) {
+                    getNotes = [];
+                } else {
+                    getNotes = JSON.parse(notes);
+                }
+                return getNotes;
+            })
+    }
+
+    addNotes(notes) {
+        const {text, title} = notes;
+        if (!text || !title) {
+            return;
+        };
+
+        const addedNote = {
+            text, title, id: uuidPackage()
+        };
+
+        this.getNotes().then((notes) => {
+            
+        })
+    }
+
+    removeNotes() {
+
     }
 }
