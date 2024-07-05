08a7fb11d5ff356e733e3d8bf14b328d7a3481d5
AA <aalborgil002@gmail.com>
2024-07-05 13:05:03 -0400

    Added minor fixes

--- db/store.js
+++ db/store.js
@@ -1,15 +1,12 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const { v4: uuidv4 } = require('uuid');
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFile('db/db.json', 'utf8');
+        return fs.readFile('db/db.json', 'utf8');
     };
     write(note) {
-        return writeFile('db/db.json', JSON.stringify(note));
+        return fs.writeFile('db/db.json', JSON.stringify(note));
     };
 
     getNotes() {
@@ -32,7 +29,9 @@ class Store {
         };
 
         const addedNote = {
-            text, title, id: uuidv4()
+            text,
+            title,
+            id: uuidv4()
         };
 
         return this.getNotes()
